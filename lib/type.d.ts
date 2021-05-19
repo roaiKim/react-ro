35bb8882bc3f1c66502576493cf6b73b0440f41e
roaiKim <shcroai@gmail.com>
2021-05-19 14:44:58 +0800

    update

--- lib/type.d.ts
+++
@@ -1,9 +0,0 @@
-import { RouterState } from "connected-react-router";
-export interface LoadingState {
-    [loading: string]: number;
-}
-export interface State {
-    loading: LoadingState;
-    app: {};
-    router: RouterState;
-}
--- lib/util/network.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function ajax<Request, Response>(method: string, url: string, request: Request): Promise<Response>;
